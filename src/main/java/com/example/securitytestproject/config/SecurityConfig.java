92660760380b9f16676e5e61922854537d1e1889
semkharkiv <info.pelikantobacco@gmail.com>
2023-11-16 13:13:57 +0100

    Security Done ,without tests

--- src/main/java/com/example/securitytestproject/config/SecurityConfig.java
+++ src/main/java/com/example/securitytestproject/config/SecurityConfig.java
@@ -12,7 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity  //говорит Spring о том, что данный класс будет использоваться для настройки безопасности вашего веб-приложения.
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -22,16 +22,18 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain defaultSecurity(HttpSecurity http) throws Exception {
         return http
-                .httpBasic(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())  // Базовая аутентификация предоставляет простой механизм аутентификации, предлагающий пользователю имя пользователя и пароль.
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/").permitAll() //Разрешает все запросы на корневой URL без аутентификации.
                         .requestMatchers(HttpMethod.POST, "/clients/create-user").permitAll()
                         .requestMatchers(HttpMethod.GET, "/clients/{id}").authenticated()
                         .requestMatchers(HttpMethod.POST, "/clients/create-manager").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/clients/manager/{name}").hasRole("ADMIN")
-                        .anyRequest().permitAll() // -это тоже не особо надо,так как у меня нету более котроллеров
+                        .anyRequest().permitAll()
                 )
-                .csrf(AbstractHttpConfigurer::disable)
-                .build();
+                .csrf(AbstractHttpConfigurer::disable)  //Отключает защиту от подделки межсайтовых запросов (CSRF). CSRF-атака – это атака, при которой злоумышленник использует учетные данные пользователя без его разрешения.
+                .build(); // Завершает конфигурацию и создает SecurityFilterChain
+
+
     }
 }
